5e84f77c0a52642d6ea9514afcc4f632ddffc04d
rhalsemd <rhalsemd@naver.com>
2023-04-08 21:57:50 +0900

    20230408 문제 풀이

---
+++ CodingTestPractice/Programmers/가장큰수찾기.java
@@ -0,0 +1,26 @@
+package example;
+
+public class 가장큰수찾기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int[] solution(int[] array) {
+        int[] answer = new int [2];
+        
+        int max = 0;
+		int index = 0;
+		for(int i =0; i<array.length; i++) {
+			if( max < array[i]) {
+				max = array[i];
+				index = i;
+			}
+		}
+        answer[0] = max;
+        answer[1] = index;
+      
+        
+        return answer;
+    }
+}
