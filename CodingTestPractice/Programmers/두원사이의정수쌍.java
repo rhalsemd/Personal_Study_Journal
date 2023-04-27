32f51f9480c9fdf9d713b5f17e43422eb72f22ce
rhalsemd <rhalsemd@naver.com>
2023-04-27 23:08:50 +0900

    20230427 문제 풀이

---
+++ CodingTestPractice/Programmers/두원사이의정수쌍.java
@@ -0,0 +1,32 @@
+package example;
+
+public class 두원사이의정수쌍 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		solution(2,3);
+	}
+	public static long solution(long r1, long r2) {
+	    long answer = 0;
+
+	    for (int i = 1; i < r2; i++) {
+	        if (i < r1) {
+	            answer += getMaxY(i, r2, "r2") - getMaxY(i, r1, "r1");
+	        } else {
+	            answer += getMaxY(i, r2, "r2");
+	        }
+	    }
+	    answer *= 4;
+	    answer += (r2 - r1 + 1) * 4;
+	    return answer;
+	}
+	private static int getMaxY(long x, long r, String rName) {
+        double max = Math.sqrt(r * r - x * x);
+        int maxToInt = (int) max;
+        if (rName.equals("r1") && max - maxToInt == 0.0) {
+            return maxToInt - 1;
+        } else {
+            return maxToInt;
+        }
+    }
+}
