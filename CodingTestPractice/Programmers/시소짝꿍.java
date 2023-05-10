4dfeb177f9b0874d4fce1f3821f644d184d30c2a
rhalsemd <rhalsemd@naver.com>
2023-05-11 00:06:27 +0900

    20230511 문제 풀이

---
+++ CodingTestPractice/Programmers/시소짝꿍.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.util.*;
+
+public class 시소짝꿍 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	public long solution(int[] weights) {
+        Map<Double, Integer> hm = new HashMap<>();
+		long ret = 0;
+        Arrays.sort(weights);
+		for(int weight : weights) {
+			ret += helper(weight, hm);
+		}
+		return ret;
+    }
+	
+	public long helper(int w, Map<Double, Integer> hm) {
+		long ret = 0;
+		double d1 = w*1.0;
+		double d2 = (w*2.0)/3.0;
+		double d3 = (w*1.0)/2.0;
+		double d4 = (w*3.0)/4.0;
+		if(hm.containsKey(d1)) ret += hm.get(d1);
+		if(hm.containsKey(d2)) ret += hm.get(d2);
+		if(hm.containsKey(d3)) ret += hm.get(d3);
+		if(hm.containsKey(d4)) ret += hm.get(d4);
+		hm.put(w*1.0, hm.getOrDefault(w*1.0, 0)+1);
+		return ret;
+	}
+}
