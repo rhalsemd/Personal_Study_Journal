0290e6b78d35419c82c03ff3664cecb8ca147e18
rhalsemd <rhalsemd@naver.com>
2023-06-07 10:49:29 +0900

    20230607 문제 풀이

---
+++ CodingTestPractice/Programmers/택배상자.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] wantOrder) {
+        int answer = 0;
+
+        Stack<Integer> saveOrder = new Stack<Integer>();
+
+        int defaultOrder = 1;
+        int wantOrderIdx = 0;
+
+        while (true) {
+            if (!saveOrder.isEmpty() && wantOrder[wantOrderIdx] == saveOrder.peek()) {
+                answer++;
+
+                wantOrderIdx++;
+                saveOrder.pop();
+
+                continue;
+            }
+
+            if (defaultOrder > wantOrder.length)
+                break;
+
+            if (wantOrder[wantOrderIdx] == defaultOrder) {
+                answer++;
+
+                wantOrderIdx++;
+                defaultOrder++;
+
+                continue;
+            }
+
+            saveOrder.push(defaultOrder);
+            defaultOrder++;
+        }
+
+        return answer;
+    }
+}
