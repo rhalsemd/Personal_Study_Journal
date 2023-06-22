89e4f381e76d9c8c1b9cd3ec3ebfbd88b875053b
rhalsemd <rhalsemd@naver.com>
2023-06-22 14:56:36 +0900

    20230622 문제 풀이

---
+++ CodingTestPractice/Backjoon/평균은 넘겠지 4344.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int[] arr;
+		
+		int testcase = in.nextInt();
+		
+		for(int i = 0 ; i < testcase ; i++) {
+ 
+			int N = in.nextInt();	
+			arr = new int[N];
+			
+			double sum = 0;
+
+			for(int j = 0 ; j < N ; j++) {
+				int val = in.nextInt();
+				arr[j] = val;
+				sum += val;
+			}
+			
+			double mean = (sum / N) ;
+			double count = 0; 
+			
+			// 평균 넘는 학생 비율 찾기 
+			for(int j = 0 ; j < N ; j++) {
+				if(arr[j] > mean) {
+					count++;
+				}
+			}
+			
+			System.out.printf("%.3f%%\n",(count/N)*100);
+			
+	
+		}
+		in.close();
+	}
+	
+}
